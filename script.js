5988686e5b2d1903b5d369168352e1463a112883
jovasal <jovasal@gmail.com>
2023-07-05 14:38:56 -0500

    speakers Dinamic
    ok

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*  ----- menu  -----  */
+
 const menuHamburger = document.querySelector('.hamburguer');
 const navMobile = document.querySelector('.navbar2023');
 const navMobileLinks = document.querySelectorAll('.nav-list2023 a');
@@ -7,7 +9,7 @@ function toggleMenu() {
   if (navMobile.classList.contains('active')) {
     hamburgerImage.src = './images/cancel.svg';
   } else {
-    hamburgerImage.src = "./images/mobile_menu 1.png";
+    hamburgerImage.src = './images/mobile_menu 1.png';
   }
 }
 
@@ -21,4 +23,33 @@ navMobileLinks.forEach((link) => {
     navMobile.classList.remove('active');
     toggleMenu();
   });
+});
+
+/*  ----- speakers  -----  */
+
+const speakersContainer = document.getElementById('speakersContainer');
+const seeMoreBtn = document.getElementById('seeMoreBtn');
+const speakerInfos = Array.from(document.getElementsByClassName('speakerInfo'));
+
+//  Delete all speakers
+for (let i = 0; i < 6; i++) {
+  speakersContainer.removeChild(speakersContainer.firstChild);
+}
+for (let i = 0; i < 6; i++) {
+  speakersContainer.removeChild(speakersContainer.firstChild);
+}
+
+// Show the firs 2 speakers
+const initialSpeakerInfos = speakerInfos.slice(0, 2);
+initialSpeakerInfos.forEach((speakerInfo) => {
+  speakersContainer.appendChild(speakerInfo.cloneNode(true));
+});
+
+// Button control
+seeMoreBtn.addEventListener('click', () => {
+  const remainingSpeakerInfos = speakerInfos.slice(2);
+  remainingSpeakerInfos.forEach((speakerInfo) => {
+    speakersContainer.appendChild(speakerInfo.cloneNode(true));
+  });
+  seeMoreBtn.style.display = 'none';
 });
